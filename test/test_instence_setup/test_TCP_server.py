82de42dea53c4ae3fea30b965ccc0cd4190a816f
F18-Maverick <xu_ruihong2009@163.com>
2026-05-29 21:54:19 +0800

    tests: move the instences setup tests to anothor dictionaries

---
+++ test/test_instence_setup/test_TCP_server.py
@@ -0,0 +1,10 @@
+import os
+import sys
+package_dictionary=os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+if package_dictionary not in os.sys.path:
+    sys.path.insert(0, package_dictionary)
+from src import connect_tcp
+def test_TCP_server():
+    connect_tcp.TCP_Server_Base()
+test_TCP_server()
+
